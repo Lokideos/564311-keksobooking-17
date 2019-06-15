9d902d00bb6a0c076c3de37c51454143debd6081
lokideos <archmage09@gmail.com>
2019-06-15 19:21:37 +0300

    Generate mock ads data

---
+++ js/main.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var AVATAR_LINK_TEMPLATE = 'img/avatars/user0';
+var AVATAR_EXTENSION = '.png';
+var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var MIN_Y = 130;
+var MAX_Y = 630;
+var overlaySelector = '.map__overlay';
+
+
+// Support
+var pickRandomIndex = function (length) {
+  return Math.floor(Math.random() * length);
+};
+
+// DOM manipulation
+var getOverlayWidth = function (overlay) {
+  return document.querySelector(overlay).offsetWidth;
+};
+
+// Generators
+// Mock helpers
+var generateX = function () {
+  return Math.floor(Math.random() * getOverlayWidth(overlaySelector));
+};
+
+var generateY = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+var generateAvatarId = function (maxId) {
+  return Math.floor(Math.random() * maxId + 1);
+};
+
+var generateAvatarLink = function () {
+  return AVATAR_LINK_TEMPLATE + generateAvatarId(8) + AVATAR_EXTENSION;
+};
+
+var generateOfferType = function () {
+  return OFFER_TYPES[pickRandomIndex(OFFER_TYPES.length)];
+};
+
+var generateLocation = function () {
+  return {
+    'x': generateX(),
+    'y': generateY(MIN_Y, MAX_Y)
+  };
+};
+
+var generateAd = function () {
+  return {
+    'author': {
+      'avatar': generateAvatarLink()
+    },
+    'offer': {
+      'type': generateOfferType()
+    },
+    'location': generateLocation()
+  };
+};
+
+var generateAdArray = function (length) {
+  var ads = [];
+  for (var i = 0; i < length; i++) {
+    ads.push(generateAd());
+  }
+
+  return ads;
+};
+
+// Runtime
+generateAdArray(8);
