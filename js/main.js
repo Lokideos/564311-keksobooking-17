2d511388bb8647e6279e4351fad9123e6ce32458
lokideos <archmage09@gmail.com>
2019-06-20 21:33:02 +0300

    Activate form on mousedown event; move main pin on mouse movement event

--- js/main.js
+++ js/main.js
@@ -42,10 +42,42 @@ var MIN_Y = 130;
 var MAX_Y = 630;
 
 // Event Handlers Functions
-var onMainPinClick = function () {
+var onMainPinMousedown = function (evt) {
   enableForms(FORMS);
   disableMapFade(map);
   activateAdForm(adForm);
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 };
 
 var onMainPinMouseup = function () {
@@ -254,7 +286,7 @@ var fillApplictationWithMocData = function () {
 };
 
 var applyEventHandlers = function () {
-  mainPin.addEventListener('click', onMainPinClick);
+  mainPin.addEventListener('mousedown', onMainPinMousedown);
   mainPin.addEventListener('mouseup', onMainPinMouseup);
   appartmentType.addEventListener('change', onAppartmentTypeChange);
   timeIn.addEventListener('change', onTimeInChange);
