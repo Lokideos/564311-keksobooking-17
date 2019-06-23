ccea8b9159c643209f025e6aeb5d42da900017f3
lokideos <archmage09@gmail.com>
2019-06-23 19:30:51 +0300

    Load information about ads from the server

--- js/data.js
+++ js/data.js
@@ -1,35 +1,22 @@
 'use strict';
 
 (function () {
+  // Initialize
   // Selectors
-  var overlaySelector = '.map__overlay';
   var pinTemplateSelector = '#pin';
   var pinFragmentSelector = '.map__pin';
   var pinsPlacementSelector = '.map__pins';
 
-  // Mock data
-  var AVATAR_LINK_TEMPLATE = 'img/avatars/user0';
-  var AVATAR_EXTENSION = '.png';
-  var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-  var MIN_Y = 130;
-  var MAX_Y = 630;
+  // Initial data
+  var PIN_PSEUDO_ELEMENT_HEIGHT = 30;
 
   // DOM manipulation
-  var renderPins = function (pinsPlacement, pinsData, fragment) {
-    var canvas = document.querySelector(pinsPlacement);
-    pinsData.forEach(function (pin) {
-      fragment.appendChild(pin);
-    });
-
-    canvas.appendChild(fragment);
-  };
-
   var adjustXLocation = function (xCoordinate) {
     return xCoordinate + document.querySelector('.map__pin').offsetWidth / 2;
   };
 
   var adjustYLocation = function (yCoordinate) {
-    return yCoordinate + document.querySelector('.map__pin').offsetHeight;
+    return yCoordinate + document.querySelector('.map__pin').offsetHeight - PIN_PSEUDO_ELEMENT_HEIGHT;
   };
 
   // Support
@@ -39,76 +26,24 @@
       .querySelector(templateFragment);
   };
 
-  var pickRandomIndex = function (length) {
-    return Math.floor(Math.random() * length);
-  };
-
-  var getOverlayWidth = function (overlay) {
-    return document.querySelector(overlay).offsetWidth;
-  };
-
-  // Mock data generation functions
-  var generateX = function () {
-    var xCoordinate = Math.floor(Math.random() * getOverlayWidth(overlaySelector));
-
-    if (xCoordinate > 1100) {
-      return 1100;
-    }
-
-    if (xCoordinate < 100) {
-      return 100;
-    }
-
-    return xCoordinate;
-  };
-
-  var generateY = function (min, max) {
-    var yCoordinate = Math.floor(Math.random() * (max - min) + min);
-
-    if (yCoordinate > 550) {
-      return 550;
-    }
-
-    return yCoordinate;
-  };
-
-  var generateAvatarId = function (maxId) {
-    return Math.floor(Math.random() * maxId + 1);
-  };
-
-  var generateAvatarLink = function () {
-    return AVATAR_LINK_TEMPLATE + generateAvatarId(8) + AVATAR_EXTENSION;
-  };
-
-  var generateOfferType = function () {
-    return OFFER_TYPES[pickRandomIndex(OFFER_TYPES.length)];
-  };
-
-  var generateLocation = function () {
-    return {
-      'x': generateX(),
-      'y': generateY(MIN_Y, MAX_Y)
-    };
-  };
-
   // Data generation
-  var generateAd = function () {
+  var generateAd = function (ad) {
     return {
       'author': {
-        'avatar': generateAvatarLink()
+        'avatar': ad.author.avatar
       },
       'offer': {
-        'type': generateOfferType()
+        'type': ad.offer.type
       },
-      'location': generateLocation()
+      'location': ad.location
     };
   };
 
-  var generateAdsArray = function (length) {
+  var generateAdsArray = function (adsData) {
     var ads = [];
-    for (var i = 0; i < length; i++) {
-      ads.push(generateAd());
-    }
+    adsData.forEach(function (ad) {
+      ads.push(generateAd(ad));
+    });
 
     return ads;
   };
@@ -129,13 +64,20 @@
     });
   };
 
-  // Runtime
-  var fillApplictationWithMockData = function () {
-    var mockAds = generateAdsArray(8);
-    var mockPins = generatePinsArray(mockAds);
-    var fragment = document.createDocumentFragment();
-    renderPins(pinsPlacementSelector, mockPins, fragment);
+  var renderPins = function (pinsPlacement, pinsData, fragment) {
+    var canvas = document.querySelector(pinsPlacement);
+    pinsData.forEach(function (pin) {
+      fragment.appendChild(pin);
+    });
+
+    canvas.appendChild(fragment);
   };
 
-  fillApplictationWithMockData();
+  // Runtime
+  window.xhr.load(function (data) {
+    var ads = generateAdsArray(data);
+    var pinsData = generatePinsArray(ads);
+    var fragment = document.createDocumentFragment();
+    renderPins(pinsPlacementSelector, pinsData, fragment);
+  });
 })();
